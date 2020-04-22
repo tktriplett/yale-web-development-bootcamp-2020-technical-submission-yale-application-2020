78e7dd48e6f296d4c27904096c242e89605f034e
Taylor Triplett <taylor.triplett@yale.edu>
2020-04-22 16:08:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 name="Taylor Triplett"
-age="19
-age_in_30_years="
+age="19"
+age_in_30_years="age+30"
+
 # Your Code Here!
 
 ## Do not modify below this line!
