22936e9c46fd62233e51260198ac263524da4032
Taylor Triplett <taylor.triplett@yale.edu>
2020-04-22 16:39:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ age="19"
 age_in_30_years="49"
 favorite_food="falafel"
 height_message= "height_in_inches" >= "67" ? "You are taller than average" : "You are not taller than average"
-message = "Hello, #{name}! You are #{age} years old. In 30 years you will be #{age_in_thirty_years} years old. Your favorite food is #{favorite_food}. That sounds tasty, but don't take my word for it - I'm just a computer! #{height_message}"
+message = "Hello, #{name}! You are #{age} years old. In 30 years you will be #{age_in_30_years} years old. Your favorite food is #{favorite_food}. That sounds tasty, but don't take my word for it - I'm just a computer! #{height_message}"
   puts message
 end
 ## Do not modify below this line!
